c84520cb0b288a1fabde9e48d0ac09e1c7819655
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-15 20:42:37 +0430

    ADD ROUTE FOR CREATE LIKE

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get '/posts/new', to: 'posts#new', as: 'create_post'
   post '/posts/new', to: 'posts#create'
   post '/users/:user_id/posts/:id/', to: 'posts#create_comment'
- 
+  post '/users/:user_id/posts/:id/likes', to: 'posts#create_like', as: 'like'
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
