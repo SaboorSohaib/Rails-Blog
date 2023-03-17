879512cbf7c2d503a9b20ab218f8f62ba8f88a1c
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-17 21:55:36 +0430

    ADD CRETAE AND NEW ACTION FOR POST

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,39 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+  def create
+    @post = Post.new(post_params)
+    @post.AuthorId = current_user.id
+    @post.like_counter = 0
+    @post.comment_counter = 0
+    return unless @post.save
+
+    redirect_to posts_new_path
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create_comment
+    @comment = Comment.new(comment_params)
+    @comment.post = Post.find(params[:id])
+    @comment.AuthorId = current_user.id
+    redirect_to user_post_path if @comment.save
+  end
+
+  def create_like
+    @like = Like.new(AuthorId: current_user.id, post: Post.find(params[:id]))
+    redirect_to user_post_path if @like.save
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:Title, :Text)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
